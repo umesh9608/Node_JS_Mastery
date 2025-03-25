5b4ce37a1cbb951ecb6c8998bb51e8ac5cadbef1
Umesh Kumar Verma <umeshkumarhzb145@gmail.com>
2025-03-25 19:53:22 +0530

    This is second Lecture

---
+++ Lecture_2_Modules/utils.js
@@ -0,0 +1,10 @@
+ export const sum = (a,b) => a + b; 
+ const sub = (a,b) => a - b;  
+ const mul = (a,b) => a * b;  
+const div = (a,b) => a / b;  
+
+//modules.exports = {sum,sub,mul,div}  //exporting the functions
+// The above code is the content of utils.js file. It contains the functions that we want to use in our index.js file. We are exporting these functions so that we can use them in our index.js file.
+
+
+//**********now we learn import funcction*********** */
